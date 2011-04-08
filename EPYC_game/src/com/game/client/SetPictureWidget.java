260f848aed2d353747ca58c4791d841e7ab473c2
Mingxun Wang <MingMac@0-23-12-56-d8-46.dynamic.ucsd.edu>
2011-04-07 19:38:53 -0700

    continuing work

--- EPYC_game/src/com/game/client/SetPictureWidget.java
+++ EPYC_game/src/com/game/client/SetPictureWidget.java
@@ -16,10 +16,11 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class SetPictureWidget extends Composite {
 	
 	private final GreetingServiceAsync greetingService;
+	private final ArrayList<String> selected_urls;
 	
 	public SetPictureWidget(final GreetingServiceAsync greetingService){
 		this.greetingService = greetingService;
-		
+		this.selected_urls = new ArrayList<String>();
 		
 		
 		final VerticalPanel vpanel = new VerticalPanel();
@@ -32,6 +33,29 @@ public class SetPictureWidget extends Composite {
 		vpanel.add(hpanel3);
 		
 		initWidget(vpanel);
+		
+		//Callback When I click on the pictures
+		class ClickPictureHandler implements ClickHandler {
+			int index;
+			/**
+			 * Fired when the user clicks on the sendButton.
+			 */
+			
+			public void setQuery(int index){
+				this.index = index;
+			}
+			
+			public void onClick(ClickEvent event) {
+				sendNameToServer();
+			}
+
+
+			/**
+			 * Send the name from the nameField to the server and wait for a response.
+			 */
+			private void sendNameToServer() {
+			}
+		}
 		
 		
 		//Callback for when I click on a word to pull up the pictures
@@ -86,12 +110,16 @@ public class SetPictureWidget extends Composite {
 				//html.setText(result.get(0));
 				String string = result.get(0);
 				String [] split_string = string.split(" ");
+				selected_urls.clear();
 				for(int i = 0; i < split_string.length; i++){
 					final HTML phrase_word = new HTML(split_string[i]);
 					ClickPhraseHandler handler = new ClickPhraseHandler();
 					handler.setQuery(split_string[i]);
+					phrase_word.addClickHandler(handler);
 					
 					hpanel1.add(phrase_word);
+					
+					selected_urls.add("");
 				}
 			}
 		});
