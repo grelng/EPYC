72c98807e4d8d4c16e0fe0943a2d7bf9dda539e7
Greg Long <gregory.g.long@gmail.com>
2011-04-07 23:27:12 -0700

    implemented the server side sentence methods, and added toolkit functions

--- EPYC_game/src/com/game/server/GreetingServiceImpl.java
+++ EPYC_game/src/com/game/server/GreetingServiceImpl.java
@@ -111,16 +111,23 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 				.replaceAll(">", "&gt;");
 	}
 
-	ArrayList<String> UserIDs = new ArrayList<String>();
-	ArrayList<String> Sentences = new ArrayList<String>();
-	ArrayList<String> URLs = new ArrayList<String>();
+	String[] UserIDs;
+	String[] Sentences;
+	String[] URLs;
+//	ArrayList<String> UserIDs = new ArrayList<String>();
+//	ArrayList<String> Sentences = new ArrayList<String>();
+//	ArrayList<String> URLs = new ArrayList<String>();
 	int Usercount = 0;
 	int NumUsers = 0;
 	int RoundNum = 0;
+	int NumURLs = 10;
+	int sentence_ctr = 0;
+	int userids_ctr = 0;
+	int urls_ctr = 0;
 	boolean Step1Done = false;
 	
 	/**
-	 * Initialize the game.
+	 * Initialize the game; check if the first round is finished
 	 * 
 	 * @param input ArrayList of strings - always in this order:
 	 * User ID
@@ -132,20 +139,27 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 	public ArrayList<String> PushPhraseInit(ArrayList<String> input)
 			throws IllegalArgumentException {
 
-		if(!Sentences.contains(input.get(2))) {
-			Sentences.add(input.get(2));
-		}
-		if(!UserIDs.contains(input.get(0))) {
-			UserIDs.add(input.get(0));
-			UserIDs.add(Integer.toString(Usercount++));
-		}
 		if(NumUsers == 0) {
 			NumUsers = Integer.parseInt(input.get(1));
+			Sentences = new String[(int) (Usercount*Math.ceil(Usercount/2))];
+			URLs = new String[(int) (Usercount*Math.ceil(Usercount/2)*NumURLs)];
+			UserIDs = new String[2*Usercount];
 		}
-
-		// Check if UserIDs contains as many users as NumUsers
-		if(NumUsers > 0 && Sentences.size() == NumUsers) {
-			Step1Done = true;
+		
+		Sentences[sentence_ctr++] = input.get(2);
+		UserIDs[userids_ctr++] = input.get(2);
+		// Check if we've inputed all the sentences from the first round
+		Step1Done = true;
+		if(NumUsers > 0) {
+			for(int i=0; i<NumUsers;i++) {
+				if(Sentences[i] == null) {
+					Step1Done = false;
+				}
+			}
+			
+		}
+		else {
+			Step1Done = false;
 		}
 		return null;
 	}
@@ -165,12 +179,23 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		return null;
 	}
 
+	/**
+	 * returns the sentence that the user is going to be drawing pictures for
+	 * 
+	 * @param input - the userid of the person that wants a sentence(?)
+	 * @return ret - an ArrayList<String> of one element size, the sentence
+	 */
 	@Override
 	public ArrayList<String> GetPhraseGame(ArrayList<String> input)
 			throws IllegalArgumentException {
 		// TODO Auto-generated method stub
 		ArrayList<String> ret = new ArrayList<String>();
-		ret.add("Ming Wang");
+//		ret.add("Ming Wang");
+		//whoseSheet() will return a string that tells me whose sheet to read from
+		String sheet = "";
+		int round = calcRound();
+		int index = findItem(sheet,UserIDs) + 1;
+		ret.add(Sentences[index*NumUsers+round]);
 		return ret;
 	}
 
@@ -253,13 +278,62 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
         return results;
 	}
 
+	/**
+	 * stores the sentence submitted by the user in the ArrayList Sentences
+	 * Sentences is structured as [user 0's sentences, user 1's sentences, ...]
+	 * @param input - ArrayList of strings, in the order Sentence, uid
+	 * @return none
+	 */
 	@Override
 	public ArrayList<String> SubmitPhraseGame(ArrayList<String> input)
 			throws IllegalArgumentException {
 		// TODO Auto-generated method stub
+		//whoseSheet() will return a string that tells me whose sheet to write to
+		String sheet = "";
+		int round = calcRound();
+		int index = findItem(sheet,UserIDs) + 1;
+		Sentences[index*NumUsers+round] = input.get(0);
 		return null;
 	}
 
+	/**
+	 * linear search through an array to find the index of an item
+	 * @param item - the item to search for
+	 * @param input - the input array
+	 * @return index of the item
+	 */
+	public int findItem(String item,String[] input) {
+		for(int i=0; i<input.length;i++) {
+			if(input[i].equals(item)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * this method calculates the round we're on based on the Sentence and URL arrays.
+	 * If this works the first time Greg is a fucking badass
+	 * @return the round number we're on, starting at 1
+	 */
+	private int calcRound() {
+		int sentence_rounds = 0;
+		String counter = Sentences[0];
+		while(!counter.equals(null)) {
+			sentence_rounds++;
+			counter = Sentences[sentence_rounds];
+		}
+		sentence_rounds = (int)Math.floor((sentence_rounds+1)/(NumUsers));
+		int pic_rounds = 0;
+		counter = URLs[0];
+		while(!counter.equals(null)) {
+			pic_rounds++;
+			counter = URLs[pic_rounds];
+		}
+		pic_rounds = (int)Math.floor((pic_rounds+1)/(NumUsers*NumURLs));
+		return sentence_rounds+pic_rounds+1;
+	}
+	
 	@Override
 	public ArrayList<String> IsStep4Done(ArrayList<String> input)
 			throws IllegalArgumentException {
